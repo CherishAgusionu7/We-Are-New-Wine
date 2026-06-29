c1c03570e3fa3a903bb3f2c762b18233f61bd457
CherishAgusionu7 <caa0365@students.untsystem.edu>
2026-06-29 06:17:35 +0000

    Add GitHub Actions workflow for deploying Vite site to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/We-Are-New-Wine/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
